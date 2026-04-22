120acc41331b22221c86ab73c3c6885354c11684
Амира Асанова <amiraasanova443@gmail.com>
2026-04-22 14:39:12 +0600

    fix vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "/",
+    base: "/electrolab_2/",
 
     plugins: [react(), tailwindcss()],
 
